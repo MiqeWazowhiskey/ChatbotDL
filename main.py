411655fe321eb3e30bdbc26790561e1d540b8bad
Ali Kağan Yılmaz <kaganylmz12@gmail.com>
2025-04-12 00:39:29 +0300

    feat: handler process (train,save model, return message)

--- main.py
+++ main.py
@@ -78,3 +78,88 @@ class ChatbotHandler:
                     self.documents.append((ptrn_words, intent['tag']))
 
                 self.vocabulary = sorted(set(self.vocabulary))
+
+    def prep_data(self):
+        bags = []
+        indices = []
+
+        for document in self.documents:
+            words = document[0]
+            bag = self.BoW(words, self.vocabulary)
+
+            intent_index = self.intents.index(document[1])
+            bags.append(bag)
+            indices.append(intent_index)
+
+        self.X = np.array(bags)
+        self.y = np.array(indices)
+
+    def train(self,batch_size, lr, epochs):
+        X_tensor = torch.tensor(self.X, dtype=torch.float32)
+        y_tensor = torch.tensor(self.y, dtype=torch.long)
+        dataset = TensorDataset(X_tensor, y_tensor)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+        self.model = ChatbotModel(self.X.shape[1],len(self.intents))
+
+        #loss function
+        criterion = nn.CrossEntropyLoss()
+        #optimizer
+        optimizer = optim.Adam(self.model.parameters(), lr=lr)
+
+        #train
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for batch_X, batch_y in dataloader:
+                optimizer.zero_grad()
+                output = self.model(batch_X)
+                loss = criterion(output, batch_y)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(dataloader):.4f}")
+        print("Training complete.")
+
+
+    def save_model(self,  model_path, dimensions_path):
+        torch.save(self.model.state_dict(), model_path)
+
+        with open(dimensions_path, 'w') as file:
+            json.dump({
+                'input_size': self.X.shape[1],
+                'output_size': len(self.intents),
+                'vocabulary': self.vocabulary,
+                'intents': self.intents
+            }, file)
+
+    def load_model(self, model_path, dimensions_path):
+        with open(dimensions_path, 'r') as file:
+            dimensions = json.load(file)
+
+        self.model = ChatbotModel(dimensions['input_size'], dimensions['output_size'])
+        self.model.load_state_dict(torch.load(model_path, weights_only=True))
+
+
+    def process_message(self, message):
+        #tokenize
+        words = self.tokenize(message)
+        #bag of words
+        bag = self.BoW(words, self.vocabulary)
+        #convert to tensor
+        bag_tensor = torch.tensor(bag, dtype=torch.float32)
+        self.model.eval()
+        with torch.no_grad():
+            predictions = self.model(bag_tensor)
+
+        predicted_class_index = torch.argmax(predictions,dim=1).item()
+        predicted_intent = self.intents[predicted_class_index]
+
+        if self.function_mappings:
+            if predicted_intent in self.function_mappings:
+                self.function_mappings[predicted_intent]()
+
+        if self.intents_responses[predicted_intent]:
+            return random.choice(self.intents_responses[predicted_intent])
+        else:
+            return None
